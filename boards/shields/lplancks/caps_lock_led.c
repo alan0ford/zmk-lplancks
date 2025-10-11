275dbacad9f26127064783ea0d58f9f73dffbc70
alan0ford <xnews@libero.it>
2025-10-11 23:04:55 +0200

    new

--- boards/shields/lplancks/caps_lock_led.c
+++ boards/shields/lplancks/caps_lock_led.c
@@ -1,34 +1,44 @@
-#include <zephyr/kernel.h>
 #include <zephyr/device.h>
-#include <zephyr/init.h> // Header necessario per SYS_INIT
-#include <zmk/behaviors/behavior_rgb_underglow.c>
+#include <zephyr/drivers/led_strip.h>
 #include <zmk/event_manager.h>
 #include <zmk/events/hid_indicators_changed.h>
-#include <zmk/hid_indicators.h>
-
 #include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(caps_led_debug, LOG_LEVEL_DBG);
+
+LOG_MODULE_REGISTER(caps_lock_led, LOG_LEVEL_DBG);
 
 static const struct device *led_strip = DEVICE_DT_GET(DT_ALIAS(smartled));
-static const struct led_rgb COLOR_CAPS_ON = { .r = 0xFF, .g = 0xFF, .b = 0xFF };
-static const struct led_rgb COLOR_CAPS_OFF = { .r = 0x00, .g = 0x00, .b = 0x00 };
+
+static void set_led_white(bool on) {
+    if (!device_is_ready(led_strip)) {
+        LOG_ERR("LED strip device non pronto");
+        return;
+    }
+    uint8_t color[3] = {0, 0, 0};
+    if (on) {
+        // Colore bianco: massimo rosso, verde e blu
+        color[0] = 0xFF; // R
+        color[1] = 0xFF; // G
+        color[2] = 0xFF; // B
+    }
+    // Scrivi colore al primo LED della striscia
+    int err = led_strip_write(led_strip, 0, 1, color);
+    if (err) {
+        LOG_ERR("Errore scrittura LED: %d", err);
+    }
+}
 
 static int caps_lock_led_listener_cb(const zmk_event_t *eh) {
-    LOG_DBG("Evento HID indicator ricevuto!");
-
-    if (!device_is_ready(led_strip)) {
-        LOG_ERR("Dispositivo LED 'smartled' non pronto.");
-        return ZMK_EV_EVENT_BUBBLE;
-    }
+    LOG_DBG("Evento HID indicator ricevuto");
 
     zmk_hid_indicators_t indicators = zmk_hid_indicators_get_current_profile();
+    bool caps_on = (indicators & HID_USAGE_LED_CAPS_LOCK) != 0;
 
-    if (indicators & HID_USAGE_LED_CAPS_LOCK) {
-        LOG_INF("Caps Lock ATTIVO. Imposto il LED a bianco.");
-        zmk_rgb_underglow_set_all(led_strip, &COLOR_CAPS_ON);
+    if (caps_on) {
+        LOG_INF("Caps Lock attivo: accendo LED");
+        set_led_white(true);
     } else {
-        LOG_INF("Caps Lock NON attivo. Spengo il LED.");
-        zmk_rgb_underglow_set_all(led_strip, &COLOR_CAPS_OFF);
+        LOG_INF("Caps Lock non attivo: spengo LED");
+        set_led_white(false);
     }
 
     return ZMK_EV_EVENT_BUBBLE;
@@ -37,16 +47,10 @@ static int caps_lock_led_listener_cb(const zmk_event_t *eh) {
 ZMK_LISTENER(caps_lock_led_listener, caps_lock_led_listener_cb);
 ZMK_SUBSCRIPTION(caps_lock_led_listener, zmk_hid_indicators_changed);
 
-
-// ***************************************************************
-// NUOVA FUNZIONE DI TEST: Eseguita una sola volta all'avvio
-// ***************************************************************
-static int caps_led_init(void) {
-    // Questo messaggio DEVE comparire nei log se il file viene compilato.
-    LOG_ERR(">>> IL MIO CODICE 'caps_lock_led.c' E' STATO COMPILATO CORRETTAMENTE! <<<");
+// Funzione init per debug - sarà chiamata all'avvio
+static int caps_led_init(const struct device *dev) {
+    ARG_UNUSED(dev);
+    LOG_INF("Caps lock LED init");
     return 0;
 }
-
-// Registra la nostra funzione per essere eseguita all'avvio dell'applicazione.
 SYS_INIT(caps_led_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
-// ***************************************************************
