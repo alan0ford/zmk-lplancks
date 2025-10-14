648ed624f20d16987302701bc2c88e20a33264c9
alan0ford <xnews@libero.it>
2025-10-14 22:40:08 +0200

    26th att

--- boards/shields/lplancks/caps_lock_led.c
+++ boards/shields/lplancks/caps_lock_led.c
@@ -2,39 +2,35 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/led_strip.h>
 #include <zephyr/init.h>
+#include <zmk/rgb_underglow.h>
 #include <zmk/event_manager.h>
 #include <zmk/events/hid_indicators_changed.h>
 #include <zmk/hid_indicators.h>
 #include <zephyr/logging/log.h>
 
-LOG_MODULE_REGISTER(caps_lock_led, LOG_LEVEL_INF);
+LOG_MODULE_REGISTER(caps_led_debug, LOG_LEVEL_DBG);
 
 static const struct device *led_strip = DEVICE_DT_GET(DT_ALIAS(smartled));
-
-static void caps_lock_set_led(bool on) {
-    if (!device_is_ready(led_strip)) {
-        LOG_ERR("LED strip device non pronto");
-        return;
-    }
-
-    uint8_t color[3] = {0, 0, 0};
-    if (on) {
-        color[0] = 0xFF; // rosso
-        color[1] = 0xFF; // verde
-        color[2] = 0xFF; // blu
-    }
-
-    int rc = led_strip_write(led_strip, 0, 1, color);
-    if (rc) {
-        LOG_ERR("Errore scrittura LED strip: %d", rc);
-    }
-}
+static const struct led_rgb COLOR_CAPS_ON = { .r = 0xFF, .g = 0xFF, .b = 0xFF };
+static const struct led_rgb COLOR_CAPS_OFF = { .r = 0x00, .g = 0x00, .b = 0x00 };
 
 static int caps_lock_led_listener_cb(const zmk_event_t *eh) {
-    zmk_hid_indicators_t indicators = zmk_hid_indicators_get_current_profile();
-    bool caps_on = (indicators & HID_USAGE_LED_CAPS_LOCK) != 0;
+    LOG_DBG("Evento HID indicator ricevuto!");
 
-    caps_lock_set_led(caps_on);
+    if (!device_is_ready(led_strip)) {
+        LOG_ERR("Dispositivo LED 'smartled' non pronto.");
+        return ZMK_EV_EVENT_BUBBLE;
+    }
+
+    zmk_hid_indicators_t indicators = zmk_hid_indicators_get_current_profile();
+
+    if (indicators & HID_USAGE_LED_CAPS_LOCK) {
+        LOG_INF("Caps Lock ATTIVO. Imposto il LED a bianco.");
+        zmk_rgb_underglow_set_all(led_strip, &COLOR_CAPS_ON);
+    } else {
+        LOG_INF("Caps Lock NON attivo. Spengo il LED.");
+        zmk_rgb_underglow_set_all(led_strip, &COLOR_CAPS_OFF);
+    }
 
     return ZMK_EV_EVENT_BUBBLE;
 }
@@ -42,10 +38,9 @@ static int caps_lock_led_listener_cb(const zmk_event_t *eh) {
 ZMK_LISTENER(caps_lock_led_listener, caps_lock_led_listener_cb);
 ZMK_SUBSCRIPTION(caps_lock_led_listener, zmk_hid_indicators_changed);
 
-static int caps_lock_led_init(const struct device *dev) {
-    ARG_UNUSED(dev);
-    LOG_INF("caps_lock_led driver iniziato");
+static int caps_led_init(void) {
+    LOG_ERR(">>> PROVA DEFINITIVA: Il file 'caps_lock_led.c' E' STATO COMPILATO! <<<");
     return 0;
 }
 
-SYS_INIT(caps_lock_led_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
+SYS_INIT(caps_led_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
