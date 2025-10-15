16bbff1c880ccfd5e0631e1345b373fe1649c626
alan0ford <xnews@libero.it>
2025-10-15 23:33:46 +0200

    GH_Copilot_V01

--- boards/shields/lplancks/caps_lock_led.c
+++ boards/shields/lplancks/caps_lock_led.c
@@ -1,26 +1,21 @@
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/init.h>
-#include <zephyr/logging/log.h>
 #include <zmk/event_manager.h>
 #include <zmk/events/hid_indicators_changed.h>
 #include <zmk/hid_indicators.h>
 #include <zmk/rgb_underglow.h>
 
-LOG_MODULE_REGISTER(capslock_indicator, LOG_LEVEL_DBG);
-
-#define COLOR_OFF   {0,0,0}         // spento
-#define COLOR_ON    {240, 100, 100} // blu acceso (HSB: 240°)
+#define COLOR_OFF   {0,0,0}
+#define COLOR_ON    {240, 100, 100}
 
 static void set_capslock_led(bool caps_on) {
     struct zmk_led_hsb color = caps_on ? (struct zmk_led_hsb)COLOR_ON : (struct zmk_led_hsb)COLOR_OFF;
     zmk_rgb_underglow_set_hsb(color);
     if (caps_on) {
         zmk_rgb_underglow_on();
-        LOG_DBG("Capslock ON: LED blu acceso");
     } else {
         zmk_rgb_underglow_off();
-        LOG_DBG("Capslock OFF: LED spento");
     }
 }
 
@@ -36,8 +31,7 @@ ZMK_SUBSCRIPTION(capslock_listener, zmk_hid_indicators_changed);
 
 static int capslock_init(const struct device *dev) {
     ARG_UNUSED(dev);
-    set_capslock_led(false); // spento all'avvio
-    LOG_INF("Modulo Capslock Indicator inizializzato.");
+    set_capslock_led(false);
     return 0;
 }
 
